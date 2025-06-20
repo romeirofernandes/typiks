14db9bb8f7522890dffc17f11aecf9eb97a2daff
Romeiro <theromeirofernandes@gmail.com>
2025-06-20 23:44:20 +0530

    sign up and sign in forms added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,19 +1,17 @@
-import React from "react";
+import React, { use } from "react";
 import { Button } from "@/components/ui/Button";
 import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
   const navigate = useNavigate()
-
-  const handleStartPlayingClick = ()=> {
-    console.log("Need to add the player searching and game starting logic here")
-  }
   return (
     <>
       <div className="max-w-5xl min-h-screen flex items-center justify-center mx-auto">
         <div className="flex flex-col justify-center items-center gap-7">
           <h1 className="text-4xl">Typiks.</h1>
-          <Button variant="default" onClick={handleStartPlayingClick} className="px-8 py-6 rounded-lg text-lg font-medium">Start typing</Button>
+          <Button variant="default" onClick={()=>{
+            navigate("/signup")
+          }} className="hover:scale-[0.97] duration-200 px-8 py-6 rounded-lg text-lg font-medium">Start typing</Button>
         </div>
       </div>
     </>
