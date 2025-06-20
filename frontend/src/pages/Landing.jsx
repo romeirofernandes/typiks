83227a626c7a7af27f5e1a642b9dcba0445b7169
Romeiro <theromeirofernandes@gmail.com>
2025-06-20 21:55:11 +0530

    tailwind and fonts set up

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <>
+      <div className="max-w-5xl bg-neutral-100 h-screen flex items-center justify-center mx-auto">
+        <h1 className="text-4xl">Landing</h1>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
